02d442855ece0f6bdcc8a173f91846a4895202bb
jennifertigner <jennifertigner@gmail.com>
2016-03-09 12:06:03 -0800

    Added content to cells

--- OldPhones/ProductsTableViewController.swift
+++ OldPhones/ProductsTableViewController.swift
@@ -17,6 +17,8 @@ class ProductsTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell",
         forIndexPath: indexPath)
+        cell.textLabel?.text = "Hello friend."
+        cell.imageView?.image = UIImage(named: "image-cell1")
         return cell
     }
     
