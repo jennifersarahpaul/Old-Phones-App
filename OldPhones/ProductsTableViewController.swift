8de59be634c9899d817c6f7e87aeaac8f0256395
jennifertigner <jennifertigner@gmail.com>
2016-03-09 11:49:25 -0800

    Created table view page

---
+++ OldPhones/ProductsTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProductsTableViewController.swift
+//  OldPhones
+//
+//  Created by Jennifer Tigner on 2016-03-09.
+//  Copyright © 2016 Jennifer Tigner. All rights reserved.
+//
+
+import UIKit
+
+class ProductsTableViewController: UITableViewController {
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell",
+        forIndexPath: indexPath)
+        return cell
+    }
+    
+}
