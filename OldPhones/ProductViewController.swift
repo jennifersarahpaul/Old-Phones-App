0d5fed5e42dfe15923a769776245321ad61e224a
jennifertigner <jennifertigner@gmail.com>
2016-03-09 10:50:17 -0800

    Moved control of label and image to outlets in the .swift Product View Controller file

---
+++ OldPhones/ProductViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProductViewController.swift
+//  OldPhones
+//
+//  Created by Jennifer Tigner on 2016-03-09.
+//  Copyright © 2016 Jennifer Tigner. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+    
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productNameLabel.text = "1937 Desk Phone"
+        productImageView.image = UIImage(named: "phone-fullscreen3")
+    }
+
+}
