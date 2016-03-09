f33f4785e312c302d531aee042701204a1627cde
jennifertigner <jennifertigner@gmail.com>
2016-03-09 12:41:15 -0800

    Made product names dynamic

--- OldPhones/ProductsTableViewController.swift
+++ OldPhones/ProductsTableViewController.swift
@@ -10,18 +10,35 @@ import UIKit
 
 class ProductsTableViewController: UITableViewController {
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+    var productNames: [String]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productNames = ["1907 Wall Set", "1921 Dial Phone", "1937 Desk Set", "1984 Motorola Portable"]
     }
     
+    // number of rows
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 'if let' checks if variable exists AND assigns it to pnames
+        if let pNames = productNames {
+            return pNames.count
+        }
+        return 0
+    }
+    
+    // runs once for each row (ie. cell)
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell",
         forIndexPath: indexPath)
-        cell.textLabel?.text = "Hello friend."
+        let productName = productNames?[indexPath.row]
+        if let pName = productName {
+            cell.textLabel?.text = pName
+        }
         cell.imageView?.image = UIImage(named: "image-cell1")
         return cell
     }
     
+    // transition from table view to product view
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "ShowProduct" {
             let productVC = segue.destinationViewController as? ProductViewController
