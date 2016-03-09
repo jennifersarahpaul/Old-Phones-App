5e62403dd2745e10a2663c31992b09439f8a9f42
jennifertigner <jennifertigner@gmail.com>
2016-03-09 15:02:23 -0800

    Added a tab bar controller at the bottom including images

---
+++ OldPhones/CartTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CartTableViewController.swift
+//  OldPhones
+//
+//  Created by Jennifer Tigner on 2016-03-09.
+//  Copyright © 2016 Jennifer Tigner. All rights reserved.
+//
+
+import UIKit
+
+class CartTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        return cell
+    }
+
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+}
